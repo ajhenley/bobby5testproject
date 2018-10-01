d9c1e97331a85eb4dafdeace03738d8fcd8565de
AJ Henley <ajhenley@gmail.com>
2018-10-01 15:54:08 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Bobby5Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bobby5Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Bobby5Application.class, args);
+  }
+}
